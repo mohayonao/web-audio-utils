ae6e08bc594aa743518b46ac4524d5db6f52df4a
mohayonao <mohayonao@gmail.com>
2015-09-18 10:41:43 +0900

    fetchAudioBuffer use XMLHttpRequest instead of fetchAPI
      fetchAPI is slow.. ?

--- fetchAudioBuffer.js
+++ fetchAudioBuffer.js
@@ -1,10 +1,34 @@
 var getAudioContext = require("./getAudioContext");
 
+function fetchWithXHR(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new global.XMLHttpRequest();
+
+    xhr.open("GET", url);
+    xhr.responseType = "arraybuffer";
+
+    xhr.onload = function() {
+      resolve({
+        arrayBuffer: function() {
+          return xhr.response;
+        },
+      });
+    };
+
+    xhr.onerror = function() {
+      // TODO: error object
+      reject({});
+    };
+
+    xhr.send();
+  });
+}
+
 module.exports = function(path, audioContext) {
   audioContext = audioContext || getAudioContext();
 
   return new Promise(function(resolve, reject) {
-    global.fetch(path).then(function(res) {
+    fetchWithXHR(path).then(function(res) {
       return res.arrayBuffer();
     }).then(function(audioData) {
       audioContext.decodeAudioData(audioData, resolve, reject);
